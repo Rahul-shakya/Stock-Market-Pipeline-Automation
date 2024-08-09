e8601834181d65c1749c3b87e51179c384e64c8c
Rahul Shakya <rahul.shakya3@gmail.com>
2024-08-09 14:07:37 -0500

    Added function for fetching stock prices for APPL

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -2,7 +2,12 @@ from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from datetime import datetime
 from airflow.sensors.base import PokeReturnValue
+from airflow.operators.python import PythonOperator
 import requests
+
+from include.stock_market.tasks import _get_stock_prices
+
+SYMBOL = 'APPL'
 
 @dag(
     start_date = datetime(2024, 8, 8),
@@ -22,7 +27,19 @@ def stock_market():
         print(url)
         return PokeReturnValue(is_done = condition, xcom_value = url)
     
-    is_api_available()
+
+    get_stock_prices = PythonOperator(
+        task_id = 'get_stock_prices',
+        python_callable = _get_stock_prices,
+
+        # {{}} is airflow template variables, see JINJA templates
+        op_kwargs = {'url' : '{{ task_instance.xcom_pull(task_ids = "is_api_available") }}', 'symbol' : SYMBOL}
+    )
+
+    # when we use decorators, we need to call the fx explicitly,
+    # like we did for is_api_available, but we did not do that for
+    # get_stock_prices, because it was defined traditionally
+    is_api_available() >> get_stock_prices
 
 
 stock_market()
