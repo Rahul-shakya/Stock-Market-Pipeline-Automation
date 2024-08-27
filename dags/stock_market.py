99101cb9fbfd865de76237f44fc64dfdb1000331
Rahul Shakya <rahul.shakya3@gmail.com>
2024-08-27 15:01:29 -0500

    Added Spark Data Processing. Output BLOB data created in minIO

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -4,9 +4,10 @@ from datetime import datetime
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 import requests
+from airflow.providers.docker.operators.docker import DockerOperator
 from include.stock_market.tasks import _get_stock_prices, _store_prices
 
-SYMBOL = 'APPL'
+SYMBOL = 'AAPL'
 
 @dag(
     start_date = datetime(2024, 8, 8),
@@ -42,12 +43,31 @@ def stock_market():
 
     )
 
+    # This docker operator will trigger a Spark job hosted in a separate docker container. 
+    # We created a spark image which will run inside this container.
+    format_prices = DockerOperator(
+        task_id = 'format_prices',
+        image = 'airflow/stock-app',
+        container_name = 'format_prices',
+        api_version = 'auto',
+        auto_remove = True,
+        docker_url = 'tcp://docker-proxy:2375',   # defined in docker-compose.yml at Line 73
+        network_mode = 'container:spark-master',
+        tty = True,
+        xcom_all = False,
+        mount_tmp_dir = False,
+
+        # SPARK_APPLICATION_ARGS is used in stock_transform Line 39
+        environment = {
+            'SPARK_APPLICATION_ARGS' : '{{task_instance.xcom_pull(task_ids = "store_prices")}}'
+        }
+    )
 
 
     # when we use decorators, we need to call the fx explicitly,
     # like we did for is_api_available, but we did not do that for
     # get_stock_prices, because it was defined traditionally
-    is_api_available() >> get_stock_prices >> store_prices
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices
 
 
 stock_market()
