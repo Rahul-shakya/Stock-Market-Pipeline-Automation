a540982e4f044eac8b14728463afc2e3430aa9ad
Rahul Shakya <rahul.shakya3@gmail.com>
2024-08-28 13:06:51 -0500

    Added the functionality to load data to Postgres DB.

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -5,9 +5,16 @@ from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 import requests
 from airflow.providers.docker.operators.docker import DockerOperator
-from include.stock_market.tasks import _get_stock_prices, _store_prices
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+
+
+
+from include.stock_market.tasks import _get_stock_prices, _store_prices, _get_formatted_csv, BUCKET_NAME
 
 SYMBOL = 'AAPL'
+# BUCKET_NAME = 'stock-market'
 
 @dag(
     start_date = datetime(2024, 8, 8),
@@ -63,11 +70,35 @@ def stock_market():
         }
     )
 
+    get_formatted_csv = PythonOperator(
+        task_id = 'get_formatted_csv',
+        python_callable = _get_formatted_csv,
+        op_kwargs = {
+            'path': '{{ task_instance.xcom_pull(task_ids = "store_prices") }}'
+        }
+    )
+
+    load_to_database = aql.load_file(
+        task_id = 'load_to_database',
+        input_file = File(path = f"s3://{BUCKET_NAME}/{{{{ task_instance.xcom_pull(task_ids = 'get_formatted_csv') }}}}", conn_id = 'minio_connection'),
+        output_table = Table(
+            name = 'stock_market',
+            conn_id = 'postgres',
+            metadata = Metadata(
+                schema = 'public'
+            )
+        )
+    )
+
+
+
+
+
 
     # when we use decorators, we need to call the fx explicitly,
     # like we did for is_api_available, but we did not do that for
     # get_stock_prices, because it was defined traditionally
-    is_api_available() >> get_stock_prices >> store_prices >> format_prices
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices >> get_formatted_csv >> load_to_database
 
 
 stock_market()
